a5a0fb46cdcc083024e30cf48dd409276c55b802
rezky setiawan <rezky.setiawan85@gmail.com>
2020-03-19 12:31:47 +0700

    activate soft delete program_studi

--- app/ProgramStudi.php
+++ app/ProgramStudi.php
@@ -3,9 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ProgramStudi extends Model
 {
+    use SoftDeletes;
     protected $table = 'program_studi';
     protected $hidden = ['created_at', 'updated_at', 'deleted_at'];
     protected $guarded = ['deleted_at'];
