9ca65e46e213b419c397229a2dda087367ecba06
rezky setiawan <rezky.setiawan85@gmail.com>
2020-03-26 12:44:44 +0700

    add python engine controller

--- app/Http/Controllers/API/PythonEngineController.php
+++ app/Http/Controllers/API/PythonEngineController.php
@@ -2,15 +2,19 @@
 
 namespace App\Http\Controllers\API;
 
+use App\Dosen;
 use App\Helpers\Host;
 use App\Helpers\Request_api;
 use App\Http\Controllers\Controller;
+use App\Jadwal;
+use App\Jobs\PythonDosen;
 use App\Krs;
 use App\Matakuliah;
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\GuzzleException;
 use GuzzleHttp\Psr7\Response;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class PythonEngineController extends Controller
 {
@@ -21,7 +25,53 @@ class PythonEngineController extends Controller
      */
     public function index()
     {
-        $krs_matkul = Krs::first()->krs_matkul;
+        $job = new PythonDosen();
+        $job->dispatch('dosen');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+
+        // Validation
+        $rules = [
+            // for get kelompok
+            'kode_krs' => ['required', 'exists:krs,kode_krs,deleted_at,NULL'],
+            'min_perkelas' => ['required', 'numeric'],
+            'max_perkelas' => ['required', 'numeric'],
+            'min_perlab' => ['required', 'numeric'],
+            'max_perlab' => ['required', 'numeric'],
+
+            // for get dosen
+            'max_kelompok' => ['required', 'numeric'],
+            'crossover_rate' => ['required', 'between:0,100'],
+            'mutation_rate' => ['required', 'between:0,100'],
+            'num_generation' => ['required', 'numeric'],
+            'num_population' => ['required', 'numeric'],
+            'timeout' => ['required', 'sometimes'],
+
+            // for jadwal
+            'tahun_ajar' => ['required'],
+            'semester' => ['required', 'in:E,O'],
+        ];
+        $validator = Validator::make($request->all(), $rules);
+        if ($validator->fails()) {
+            $response = [
+                'status' => 400,
+                'message' => $validator->errors()
+            ];
+            return response()->json($response, 400);
+        }
+        // prepare for send to python engine
+        $whereCond = [
+            'kode_krs' => $request->kode_krs
+        ];
+        $krs_matkul = Krs::where($whereCond)->first()->krs_matkul;
         $peminat_params = $krs_matkul->map(function ($item, $index) {
             $item = [
                 'kode_matkul' => $item->kode_matkul,
@@ -30,6 +80,7 @@ class PythonEngineController extends Controller
             ];
             return $item;
         });
+
         $peminat_params = $peminat_params->toArray();
         $peminat_props = [
             'min_perkelas'  => 20,
@@ -42,18 +93,19 @@ class PythonEngineController extends Controller
             'peminat_props'     => $peminat_props
         ];
 
+        // send request to python engine and get kelompok
         $host = new Host();
-        $url = $host->host('python_engine') . 'bagi_kelompok';
+        $url = $host->host('python_engine') . 'kelompok';
         $client = new Client();
         try {
             $res = $client->request('POST', $url, ['json' => $form_params]);
             $contents = $res->getBody()->getContents();
             $contents = json_decode($contents);
-            $response = [
-                'status' => $res->getStatusCode(),
-                'data' => $contents
-            ];
-            return response()->json($response, 200);
+            $matkul_kelompok = collect($contents);
+            $matkul_kelompok = $matkul_kelompok->map(function ($item, $index) {
+                $item = collect($item)->toArray();
+                return $item;
+            });
         } catch (GuzzleException $e) {
             $contents = $e->getResponse()->getBody()->getContents();
             $status_code = $e->getResponse()->getStatusCode();
@@ -64,17 +116,119 @@ class PythonEngineController extends Controller
             ];
             return response()->json($response, $status_code);
         }
-    }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
+
+        // prepare to get dosen algen
+        $matkul = Matakuliah::all();
+        $dosen_by_matkul = $matkul->mapWithKeys(function ($item, $index) {
+            $dosen = $item->dosen_matkul->map(function ($item, $index) {
+                return $item->kode_dosen;
+            });
+            $dosen = $dosen->toArray();
+            return [$item->kode_matkul => $dosen];
+        });
+        $dosen_by_matkul = $dosen_by_matkul->map(function ($item, $key) {
+            $item_new = [
+                'kode_matkul' => $key,
+                'kode_dosen' => $item
+            ];
+            return $item_new;
+        })->values();
+        $dosen_by_matkul = [
+            'kode_matkul' => $dosen_by_matkul->pluck('kode_matkul'),
+            'kode_dosen' => $dosen_by_matkul->pluck('kode_dosen')
+        ];
+
+        $rules = [
+            'max_kelompok' => $request->max_kelompok
+        ];
+
+
+        $form_params = [
+            'nn_params' => [
+                'mata_kuliah' => $matkul_kelompok,
+                'matkul_dosen' => $dosen_by_matkul
+            ],
+            'rules' => $rules,
+            'num_generation' => $request->num_generation,
+            'num_population' => $request->num_population,
+            'crossover_rate' => $request->crossover_rate / 100,
+            'mutation_rate' => $request->mutation_rate / 100,
+            'timeout' => $request->timeout
+        ];
+
+        // send request to python engine and get kelompok
+        $host = new Host();
+        $url = $host->host('python_engine') . 'dosen';
+        $client = new Client();
+        $res = $client->requestAsync('POST', $url, ['json' => $form_params])->then(
+            function ($result) {
+                $contents = $result->getBody()->getContents();
+                $contents = json_decode($contents);
+                $response = [
+                    'status' => $result->getStatusCode(),
+                    'data' => $contents
+                ];
+                return $response;
+            },
+            function ($error) {
+                $contents = $error->getResponse()->getBody()->getContents();
+                $message = $error->getResponse()->getMessage();
+                $response = [
+                    'status' => $error->getStatusCode(),
+                    'message' => $message
+                ];
+                return $response;
+                // return response()->json($response, $error->getResponse()->getStatusCode());
+            }
+        );
+
+        // insert porcess start
+        $kode_jadwal_pre = date('Ymd', time());
+        $kode_jadwal = Jadwal::where('kode_jadwal', 'like', $kode_jadwal_pre . '%')->orderBy('kode_jadwal', 'desc')->first();
+        if (!is_null($kode_jadwal)) {
+            $kode_jadwal = substr($kode_jadwal->kode_jadwal, strlen($kode_jadwal_pre));
+            $kode_jadwal = (int) $kode_jadwal + 1;
+            $kode_jadwal = $kode_jadwal_pre . $kode_jadwal;
+        } else {
+            $kode_jadwal = $kode_jadwal_pre . '0';
+        }
+        try {
+            // Insert To Jadwal
+            $dataInsert = [
+                'kode_jadwal' => $kode_jadwal,
+                'tahun_ajar' => $request->tahun_ajar,
+                'semester' => $request->semester,
+                'created_at' => date('Y-m-d H:i:s', time()),
+                'updated_at' => date('Y-m-d H:i:s', time()),
+            ];
+            $insertId = Jadwal::insertGetId($dataInsert);
+        } catch (\Throwable $th) {
+            //throw $th;
+        }
+
+
+        $res = $res->wait();
+        if ($res['status'] == 200) {
+            try {
+                // update status jadwal
+                $dataUpdate = [
+                    'status_jadwal' => true
+                ];
+                $whereCond = [
+                    'kode_jadwal' => $kode_jadwal
+                ];
+                $result = Jadwal::where($whereCond)->update($dataUpdate);
+            } catch (\Throwable $th) {
+                $whereCond = [
+                    'id' => $insertId
+                ];
+                $delete = Jadwal::delete();
+                return response()->json($res, $res['status']);
+            }
+        }
+
+        return response()->json($res, 200);
     }
 
     /**
