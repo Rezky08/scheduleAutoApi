9c8a9c0b2319bf10126df6eb167aaf0f2d44a8df
root <rezky.setiawan85@gmail.com>
2020-04-18 16:17:02 +0700

    ready prepare algen jadwal

--- app/Http/Controllers/API/PythonEngineController.php
+++ app/Http/Controllers/API/PythonEngineController.php
@@ -76,7 +76,17 @@ class PythonEngineController extends Controller
             })->toArray()
         ];
         $kelompok_dosen = KelompokDosen::find($request->kelompok_dosen_id);
-        dd($combine);
+        $kelompok_dosen = $kelompok_dosen->detail->map(function ($item)
+        {
+            $item = collect($item)->except(['id','kelompok_dosen_id'])->toArray();
+            return $item;
+        })->toArray();
+        $params = [
+            'nn_params'=>[
+                'mata_kuliah' => $kelompok_dosen
+            ]
+        ];
+        $params+=$combine;
     }
 
     /**
